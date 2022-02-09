123483282f4fa0feed725944b7b889f5b62d4f64
Ayush <ayush.kanduri@gmail.com>
2022-02-09 20:15:32 +0530

    binary math added

---
+++ Javascript Extra Features/BinaryMath.js
@@ -0,0 +1,3 @@
+//Floating poitn numbers addition can result into extreme decimal values. There are some rounding errors and we don't get the exact result.
+console.log(0.1 + 0.2);
+//JS converts 0.1 to binary decimal number which is infinitely long number and same is for 0.2.
