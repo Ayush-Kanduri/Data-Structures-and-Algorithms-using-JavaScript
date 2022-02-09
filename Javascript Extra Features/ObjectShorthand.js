698ea682cabf9352fe5e66a8d92ad1466ce5c0b3
Ayush <ayush.kanduri@gmail.com>
2022-02-09 16:13:31 +0530

    object shorthand added

---
+++ Javascript Extra Features/ObjectShorthand.js
@@ -0,0 +1,14 @@
+//Object Shorthand property - helps us in writing clean and crisp code//
+//same name key value pairs are used in object//
+
+let name2 = "John";
+let gender = "male";
+let age = 26;
+
+let obj = {
+	name2,
+	sex: gender,
+	age,
+};
+
+console.log(obj);
