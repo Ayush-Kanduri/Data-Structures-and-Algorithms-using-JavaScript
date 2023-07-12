35d26fc63f13f1debfc44cf33cb1b5fe2ea4425d
Ayush <ayush.kanduri@gmail.com>
2023-07-12 20:15:49 +0530

    Add DecimalToBinary.js

---
+++ Basic Interview Problems/DecimalToBinary.js
@@ -0,0 +1,33 @@
+/**
+    * @function decimalToBinary
+    * @description Converts a decimal number to binary
+    * @param {number} decimal - The decimal number to be converted
+    * @timeComplexity O(log n)
+    * @spaceComplexity O(1)
+    * @returns {number} - The binary number
+    * @example
+    const decimal = 50;
+    console.log(decimalToBinary(decimal));
+
+    Output:
+    110010
+**/
+
+const decimalToBinary = (decimal) => {
+	if (decimal === 0) return 0;
+	if (decimal === 1) return 1;
+	let dividend = decimal;
+	let divisor = 2;
+	let remainder = [];
+	let quotient = 1;
+	do {
+		quotient = Math.floor(dividend / divisor);
+		remainder.push(dividend % divisor);
+		dividend = quotient;
+	} while (quotient > 1);
+	remainder.push(quotient);
+	return parseInt(remainder.reverse().join(""));
+};
+
+const decimal = 50;
+console.log(decimalToBinary(decimal));
