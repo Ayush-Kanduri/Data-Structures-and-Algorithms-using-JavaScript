a10921f1f3337359e4004a050e9fd87f908bf784
Ayush <ayush.kanduri@gmail.com>
2023-07-12 15:27:42 +0530

    Update FlattenArray.js

--- Basic Interview Problems/FlattenArray.js
+++ Basic Interview Problems/FlattenArray.js
@@ -24,7 +24,8 @@
                 console.log(flattenArray(arr, 4));
                 console.log(flattenArray(arr));
 
-        Output - [
+        Output:
+            [
                 1, 2, 3, 4, 5, 6, 7, 8, 
                 [9, 10], 
                 11, 12, 
