4ff1c2bc479a4a79fc359feee231ca2dc44cfc0b
Ayush <ayush.kanduri@gmail.com>
2023-07-12 21:09:39 +0530

    Update DecimalToBinary.js

--- Basic Interview Problems/DecimalToBinary.js
+++ Basic Interview Problems/DecimalToBinary.js
@@ -2,8 +2,8 @@
     * @function decimalToBinary
     * @description Converts a decimal number to binary
     * @param {number} decimal - The decimal number to be converted.
-    Decimal Base is 10. 
-    Binary Base is 2.
+    Decimal Base is 10 --> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    Binary Base is 2 --> [0, 1]
     Decimal to Binary --> 2|10 --> 2|5 (0) --> 2|2 (1) --> 2|1 (0)
     * @timeComplexity O(log n)
     * @spaceComplexity O(1)
