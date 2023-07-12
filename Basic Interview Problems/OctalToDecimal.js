9bcf62aa8020e5ea074028353d1343b0f0408c95
Ayush <ayush.kanduri@gmail.com>
2023-07-12 21:08:45 +0530

    Update OctalToDecimal.js

--- Basic Interview Problems/OctalToDecimal.js
+++ Basic Interview Problems/OctalToDecimal.js
@@ -1,8 +1,8 @@
 /**
     * @function octalToDecimal
     * @description Converts a octal number to decimal.
-    Decimal Base is 10. 
-    Octal Base is 8.
+    Decimal Base is 10 --> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    Octal Base is 8 --> [0, 1, 2, 3, 4, 5, 6, 7]
     Octal to Decimal --> (... + 0*8^2 + 1*8^1 + 0*8^0)
     * @param {number} octal - The octal number to be converted
     * @timeComplexity O(n)
