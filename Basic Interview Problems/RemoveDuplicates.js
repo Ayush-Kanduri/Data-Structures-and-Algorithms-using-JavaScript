bba70f9034062332fb18855be6edbba93fd4f945
Ayush <ayush.kanduri@gmail.com>
2023-07-14 22:45:41 +0530

    Add RemoveDuplicates.js

---
+++ Basic Interview Problems/RemoveDuplicates.js
@@ -0,0 +1,19 @@
+/**
+    * @function removeDuplicates
+    * @description This function removes the duplicate numbers from an array
+    * @param {Array} arr - Numbers
+    * @timeComplexity O(n)
+    * @spaceComplexity O(n)
+    * @returns {Array} - Unique numbers
+    * @example
+        let arr = [1, 1, 1, 2, 2, 23, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 9];
+        console.log(removeDuplicates(arr));
+
+        Output:
+        4321    
+**/
+
+const removeDuplicates = (arr) => [...new Set(arr)];
+
+let arr = [1, 1, 1, 2, 2, 23, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 9];
+console.log(removeDuplicates(arr));
