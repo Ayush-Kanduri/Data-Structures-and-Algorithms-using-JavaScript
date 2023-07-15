44ee49ec331c611de00fbff415208c8d04d51629
Ayush <ayush.kanduri@gmail.com>
2023-07-15 23:25:11 +0530

    Add ArrayToObject.js

---
+++ Basic Interview Problems/ArrayToObject.js
@@ -0,0 +1,26 @@
+/**
+    * @description Converts the key value pair Array to an Object
+    * @timeComplexity O(n)
+    * @spaceComplexity O(n)
+    * @example
+        console.log(
+            Object.fromEntries([
+                ["a", 1],
+                ["b", 2],
+                ["c", 3],
+                ["d", 4],
+            ])
+        );
+
+        Output:
+        {a: 1, b: 2, c: 3, d: 4}
+**/
+
+console.log(
+	Object.fromEntries([
+		["a", 1],
+		["b", 2],
+		["c", 3],
+		["d", 4],
+	])
+);
