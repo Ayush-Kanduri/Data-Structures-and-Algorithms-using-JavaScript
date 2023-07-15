b34c94cc365bd92919d1d6410124ba1952a056a5
Ayush <ayush.kanduri@gmail.com>
2023-07-15 20:24:45 +0530

    Update Swap2Numbers.js

--- Basic Interview Problems/Swap2Numbers.js
+++ Basic Interview Problems/Swap2Numbers.js
@@ -8,6 +8,17 @@
 
         Output: 
         a = 30, b = 20
+
+
+    * @function swap2NumbersUsingArrayDestructuring
+    * @description - Swaps two number using array destructuring
+    * @TimeComplexity - O(1)
+    * @SpaceComplexity - O(1)
+    * @example
+        swap2NumbersUsingArrayDestructuring();
+
+        Output: 
+        a = 30, b = 20
 **/
 
 function swap2Numbers() {
@@ -20,3 +31,12 @@ function swap2Numbers() {
 	console.log(`After Swap: a = ${a}, b = ${b}`);
 }
 swap2Numbers();
+
+function swap2NumbersUsingArrayDestructuring() {
+	let a = 20;
+	let b = 30;
+	console.log(`Before Swap: a = ${a}, b = ${b}`);
+	[a, b] = [b, a];
+	console.log(`After Swap: a = ${a}, b = ${b}`);
+}
+swap2NumbersUsingArrayDestructuring();
